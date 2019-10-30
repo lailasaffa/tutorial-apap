9a637672424314d0f64ce00b1536e5cbdea60015
lailasaffa <laila.saffanah@gmail.com>
2019-10-30 22:19:48 +0700

    feat:proses mengerjakan tutorial 6

---
+++ gopud/src/main/java/apap/tutorial/gopud/restcontroller/MenuRestController.java
@@ -0,0 +1,9 @@
+package apap.tutorial.gopud.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MenuRestController {
+}
